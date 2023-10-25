e9a986e2315926a7c6dca8a810daef625240e036
HoVanthang21115053120144 <hovanthang2503@gmail.com>
2023-10-25 20:37:31 +0700

    Start,Login,Signup

---
+++ app/src/main/java/com/example/traveling_app/activity_succesful.java
@@ -0,0 +1,14 @@
+package com.example.traveling_app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class activity_succesful extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_succesful);
+    }
+}
