6ba5353d32a2baca693acf54e1a2ad15b7c3dba2
qvuongg <quocvuong22052003@gmail.com>
2023-10-28 16:33:45 +0700

    Pop up nm chua xong mn ai ranh lam giup voi

--- app/src/main/java/com/example/traveling_app/Status_activity.java
+++ app/src/main/java/com/example/traveling_app/Status_activity.java
@@ -1,6 +1,7 @@
 package com.example.traveling_app;
 
 import android.Manifest;
+import android.app.Dialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
@@ -8,6 +9,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -17,10 +19,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Status_activity extends AppCompatActivity {
     private ImageView back;
+    Dialog myDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.status_main);
+        myDialog = new Dialog(this);
+        ImageView imageView = (ImageView) findViewById(R.id.imageView4);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myDialog.setContentView(R.layout.comment_main);
+                myDialog.show();
+                myDialog.setCanceledOnTouchOutside(true); // Cho phép người dùng bấm ngoài Dialog để đóng
+                myDialog.setCancelable(true); // Cho phép sử dụng phím Back để đóng Dialog
+                myDialog.getWindow().setLayout(
+                        WindowManager.LayoutParams.MATCH_PARENT,
+                        WindowManager.LayoutParams.MATCH_PARENT
+                );
+
+            }
+        });
         back=findViewById(R.id.back_detail_blog);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
